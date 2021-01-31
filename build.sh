cd084920937c56935e22fa53148a042ceb658e75
Shao Wang <shao.wang.me@gmail.com>
2021-02-01 01:14:54 +1100

    Set language as zh

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ node make_toc.js docs/ docs/index.md
 find docs/ -name "*.html" -exec rm {} \;
 
 # 改自https://gist.github.com/bzerangue/2504041
-find docs/ -name "*.md*" | while read -r i; do pandoc "$i" -s -o "${i%.*}.html" -B header.html; done
+find docs/ -name "*.md*" | while read -r i; do pandoc "$i" -s -o "${i%.*}.html" -B header.html --metadata lang=zh; done
 
 # TODO 自定义CSS
